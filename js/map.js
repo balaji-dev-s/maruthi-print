8dde633afd14d684632fb62084c622c4599fd93e
balaji-dev-s <balajishanmugam1403@gmail.com>
2026-01-17 12:09:26 +0530

    added map link

--- js/map.js
+++ js/map.js
@@ -1,14 +1,4 @@
-const companyCoords = [12.955051735379312, 80.1422212416568]; // Replace with your latitude & longitude
-
-
-const map = L.map('map').setView(companyCoords, 15);
-
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-L.marker(companyCoords).addTo(map)
-    .bindPopup('AP New Line Facade')
-    .openPopup();
+const companyCoords = [12.955051735379312, 80.1422212416568];
+const map = L.map('map').setView(companyCoords, 30);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker(companyCoords).addTo(map).bindPopup('AP New Line Facade').openPopup();
