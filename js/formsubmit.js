bd6c69757856741dc5c5b6d9d725771719a30a20
balaji-dev-s <balajishanmugam1403@gmail.com>
2026-02-06 12:43:04 +0530

    updated form validation

--- js/formsubmit.js
+++ js/formsubmit.js
@@ -2,7 +2,11 @@ document.getElementById("enquiryForm").addEventListener("submit", e => {
   e.preventDefault();
   const f = e.target;
 
-  // Capture values BEFORE reset
+  if (!f.checkValidity()) {
+    f.classList.add("was-validated");
+    return;
+  }
+
   const data = {
     name: f.name.value,
     email: f.email.value,
@@ -10,13 +14,11 @@ document.getElementById("enquiryForm").addEventListener("submit", e => {
     message: f.message.value
   };
 
-  // Instant UX
   alert("Enquiry submitted successfully");
+
   f.reset();
   f.classList.remove("was-validated");
 
-
-  // Background request (no waiting)
   fetch("https://script.google.com/macros/s/AKfycbzaDCOtoUu5GOBsW7SQUQZZsieZ172pdZFfi6kQEUohP5t7k7DKVmhAex5OiAANZOmaXw/exec", {
     method: "POST",
     mode: "no-cors",
